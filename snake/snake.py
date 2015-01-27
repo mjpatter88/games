35545b24206ac853f18a693e29d62c81b7da259a
Michael Patterson <mjpatter88@gmail.com>
2015-01-26 19:41:02 -0500

    Refactored snake.py so the game is a class that contains the game state, etc. This should allow me to easily add a tail and add it to the list of sprites, etc.

--- snake/snake.py
+++ snake/snake.py
@@ -14,6 +14,100 @@ SCREEN_HEIGHT = HEIGHT_IN_CELLS * CELL_DIM
 SPEED = 2 # Always make speed a factor of "CELL_DIM" so things stay aligned.
 START_LEN = 10 # Inlucding the head tile even though it's not part of the tail.
 
+class Game():
+    def __init__(self):
+        self.name = "Snakes on a Screen"
+        pygame.init()
+        self.screen = pygame.display.set_mode([SCREEN_WIDTH, SCREEN_HEIGHT])
+        pygame.display.set_caption(self.name)
+        self.score = 0
+        self.scoreFont = pygame.font.SysFont("monospace", 30)
+        self.running = False
+
+    def gen_rand_food_coords(self):
+        x = random.randint(0, WIDTH_IN_CELLS - 1)
+        y = random.randint(0, HEIGHT_IN_CELLS - 1)
+        x_coord = FOOD_MARGIN + x*CELL_DIM
+        y_coord = FOOD_MARGIN + y*CELL_DIM
+        return x_coord, y_coord
+
+    def menu(self):
+        '''
+        If needed, include the menu logic here. Once the user starts the game, call
+        the function to change into the game state.
+        '''
+        self.game_loop()
+
+    def game_loop(self):
+        '''
+        The main game where the player moves around the snake.
+        '''
+        self.running = True
+        clock = pygame.time.Clock()
+        sprite_list = pygame.sprite.Group()
+
+        # Add the player and the tail
+        player = Player()
+        sprite_list.add(player)
+        tail = player.create_initial_tail()
+        for piece in tail:
+            sprite_list.add(piece)
+
+
+        # Add the food
+        food = Food(*self.gen_rand_food_coords()) # * operator unpacks the tuple
+        sprite_list.add(food)
+
+        # Create the label for the score
+        score_label = self.scoreFont.render("Score: ", 1, (255, 255, 255))
+
+        # Main game loop
+        while self.running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.running = False
+
+                elif event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_LEFT:
+                        player.change_dir(3)
+                    elif event.key == pygame.K_RIGHT:
+                        player.change_dir(1)
+                    elif event.key == pygame.K_DOWN:
+                        player.change_dir(2)
+                    elif event.key == pygame.K_UP:
+                        player.change_dir(4)
+
+            player.update()
+
+            # Handle collisions with the snake
+            tail_hits = pygame.sprite.spritecollide(player, tail[1:], False)
+            # don't include the first piece of the tail
+            # the overlap isn't a real collision
+            if tail_hits:
+                self.running = False
+
+            # Handle collisions with the walls (warp or die?)
+            
+
+            # Handle collisions with the food
+            point = pygame.sprite.collide_mask(player, food)
+            # TODO: analyze the performance of this call
+            # Create a sprite mask at load time to increase performance?
+            if point:
+                food.rect.x, food.rect.y = self.gen_rand_food_coords()
+                self.score = self.score + 10
+                score_label = self.scoreFont.render(("Score: " + str(self.score)), 1, (255, 255, 255))
+                player.extend_tail()
+
+            # Draw the next frame
+            self.screen.fill((0, 0, 0))
+            sprite_list.draw(self.screen)
+            self.screen.blit(score_label, (10, 10))
+            pygame.display.flip()
+            clock.tick(60)
+
+        pygame.quit()
+
 
 class Player(pygame.sprite.Sprite):
     # 0 - no movement
@@ -21,12 +115,6 @@ class Player(pygame.sprite.Sprite):
     # 2 - down
     # 3 - left
     # 4 - up
-    cur_dir = 0
-    next_dir = 0
-    movement_clock = 0
-    movement_list = [] # Holds the past moves so the tail can follow
-    tail = [] # Holds the list of tail pieces
-    not_moved = True
 
     def __init__(self):
         super(Player, self).__init__()
@@ -35,8 +123,13 @@ class Player(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.x = MARGIN + 10*CELL_DIM
         self.rect.y = MARGIN + 10*CELL_DIM
-        self.movement_list = [0] * START_LEN
+        self.movement_list = [0] * START_LEN # Holds the past moves so the tail can follow
+        self.tail = []
         self.tail_index = 1
+        self.cur_dir = 0
+        self.next_dir = 0
+        self.movement_clock = 0
+        self.not_moved = True
 
     def create_initial_tail(self):
         # Add the beginning tail. Start with START_LEN
@@ -48,6 +141,10 @@ class Player(pygame.sprite.Sprite):
         return self.tail
 
     def extend_tail(self):
+        '''
+            This method really just sets a flag so the tail will be extended
+            at the proper time.
+        '''
         print "Tail add"
         # TODO: Start here. Create the new tail section, add it, return it, etc.
         # Give it the same movement as the old last tail section?
@@ -68,13 +165,16 @@ class Player(pygame.sprite.Sprite):
         else:
             self.movement_clock = 1
             self.cur_dir = self.next_dir
-            self.movement_list.insert(0, self.cur_dir)
 
             # Only do for the first move. Start moving right
             if self.not_moved and self.cur_dir != 0:
                 for x in xrange(START_LEN):
                     self.movement_list.insert(0, 1)
                 self.not_moved = False
+
+            # This has to take place after the initial movement list is created
+            # Otherwise the tail will always be one move behind
+            self.movement_list.insert(0, self.cur_dir)
 
 
         if self.cur_dir == 1:
@@ -97,9 +197,6 @@ class TailPiece(pygame.sprite.Sprite):
     # 2 - down
     # 3 - left
     # 4 - up
-    cur_dir = 0
-    movement_list = None
-    index = -1
 
     def __init__(self, index, x, y, moves):
         super(TailPiece, self).__init__()
@@ -110,6 +207,7 @@ class TailPiece(pygame.sprite.Sprite):
         self.rect.y = y
         self.movement_list = moves
         self.index = index
+        self.cur_dir = 0 # See class comment for list of directions and integer mappings
 
     def update(self):
         if self.movement_list[self.index] == 1:
@@ -132,98 +230,10 @@ class Food(pygame.sprite.Sprite):
         self.rect.y = y
 
 
-def rand_food_coords():
-    x = random.randint(0, WIDTH_IN_CELLS - 1)
-    y = random.randint(0, HEIGHT_IN_CELLS - 1)
-    x_coord = FOOD_MARGIN + x*CELL_DIM
-    y_coord = FOOD_MARGIN + y*CELL_DIM
-    return x_coord, y_coord
-
-def menu(screen):
-    '''
-    If needed, include the menu logic here. Once the user starts the game, call
-    the function to change into the game state.
-    '''
-    snake(screen)
-
-def snake(screen):
-    '''
-    The main game where the player moves around the snake.
-    '''
-    score = 0
-    myFont = pygame.font.SysFont("monospace", 30)
-    running = True
-    addTailPiece = False # Set to true to add a tail piece
-    clock = pygame.time.Clock()
-    sprite_list = pygame.sprite.Group()
-
-    # Add the player and the tail
-    player = Player()
-    sprite_list.add(player)
-    tail = player.create_initial_tail()
-    for piece in tail:
-        sprite_list.add(piece)
-
-
-    # Add the food
-    food = Food(*rand_food_coords()) # * operator unpacks the tuple
-    sprite_list.add(food)
-
-    # Create the label for the score
-    score_label = myFont.render("Score: ", 1, (255, 255, 255))
-
-    # Main game loop
-    while running:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-
-            elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_LEFT:
-                    player.change_dir(3)
-                elif event.key == pygame.K_RIGHT:
-                    player.change_dir(1)
-                elif event.key == pygame.K_DOWN:
-                    player.change_dir(2)
-                elif event.key == pygame.K_UP:
-                    player.change_dir(4)
-
-        player.update()
-
-        # Handle collisions with the snake
-        tail_hits = pygame.sprite.spritecollide(player, tail[1:], False)
-        # don't include the first piece of the tail
-        # the overlap isn't a real collision
-        if tail_hits:
-            running = False
-
-        # Handle collisions with the walls (warp or die?)
-        
-
-        # Handle collisions with the food
-        point = pygame.sprite.collide_mask(player, food)
-        # TODO: analyze the performance of this call
-        # Create a sprite mask at load time to increase performance?
-        if point:
-            food.rect.x, food.rect.y = rand_food_coords()
-            score = score + 10
-            score_label = myFont.render(("Score: " + str(score)), 1, (255, 255, 255))
-            player.extend_tail()
-
-        # Draw the next frame
-        screen.fill((0, 0, 0))
-        sprite_list.draw(screen)
-        screen.blit(score_label, (10, 10))
-        pygame.display.flip()
-        clock.tick(60)
-
-    pygame.quit()
 
 def main():
-    pygame.init()
-    screen = pygame.display.set_mode([SCREEN_WIDTH, SCREEN_HEIGHT])
-    pygame.display.set_caption("Snakes on a Screen")
-    menu(screen)
+    game = Game()
+    game.menu()
 
 if __name__ == '__main__':
     main()
