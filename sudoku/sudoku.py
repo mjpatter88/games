aaf5bd9d8ab14ed13105063819c03eeb534d937b
Michael Patterson <mjpatter88@gmail.com>
2016-02-21 21:19:17 -0600

    Finished functionality, added example boards.

--- sudoku/sudoku.py
+++ sudoku/sudoku.py
@@ -11,9 +11,16 @@ WIDTH = HEIGHT = MARGIN * 2 + SIDE * 9
 class SudokuError(Exception):
     pass
 
+
+def parse_arguments():
+    arg_parser = argparse.ArgumentParser()
+    arg_parser.add_argument("--board", help="Desired board name", type=str, choices=BOARDS, required=True)
+    args = vars(arg_parser.parse_args())
+    return args['board']
+
 class SudokuBoard(object):
     def __init__(self, board_file):
-        self.board = board_file
+        self.board = self.__create_board(board_file)
 
     def __create_board(self, board_file):
         board = []
@@ -30,7 +37,7 @@ class SudokuBoard(object):
             for char in line:
                 if not char.isdigit():
                     raise SudokuError("Valid characters for a sudoku puzzle must be in 0-9.")
-                board[-1].append(int(c))
+                board[-1].append(int(char))
         
         if len(board) != 9:
             raise SudokuError("Each sudoku puzzle must be 9 lines long")
@@ -86,14 +93,14 @@ class SudokuUI(Frame):
     def __init__(self, parent, game):
         self.game = game
         self.parent = parent
-        frame.__init__(self, parent)
+        Frame.__init__(self, parent)
 
         self.row, self.col = 0, 0
         self.__initUI()
 
     def __initUI(self):
         self.parent.title("Sudoku")
-        self.pack(fille=BOTH, expand=1)
+        self.pack(fill=BOTH, expand=1)
         self.canvas = Canvas(self, width=WIDTH, height=HEIGHT)
         self.canvas.pack(fill=BOTH, side=TOP)
         clear_button = Button(self, text="Clear answers", command=self.__clear_answers)
@@ -113,15 +120,82 @@ class SudokuUI(Frame):
             y0 = MARGIN
             x1 = MARGIN + i * SIDE
             y1 = HEIGHT - MARGIN
-            self.canvas.create_line(x0, y0, x1 y1, fill=color)
+            self.canvas.create_line(x0, y0, x1, y1, fill=color)
 
             x0 = MARGIN
             y0 = MARGIN + i * SIDE
             x1 = WIDTH - MARGIN
             y1 = MARGIN + i * SIDE
-            self.canvas.create_line(x0, y0, x1 y1, fill=color)
+            self.canvas.create_line(x0, y0, x1, y1, fill=color)
 
     def __draw_puzzle(self):
         self.canvas.delete("numbers")
+        for i in xrange(9):
+            for j in xrange(9):
+                answer = self.game.puzzle[i][j]
+                if answer != 0:
+                    x = MARGIN + j * SIDE + SIDE / 2
+                    y = MARGIN + i * SIDE + SIDE / 2
+                    original = self.game.start_puzzle[i][j]
+                    color = "black" if answer == original else "sea green"
+                    self.canvas.create_text(x, y, text=answer, tags="numbers", fill=color)
 
+    def __clear_answers(self):
+        self.game.start()
+        self.canvas.delete("victory")
+        self.__draw_puzzle()
+
+    def __cell_clicked(self, event):
+        if self.game.game_over:
+            return
+
+        x, y = event.x, event.y
+        if (MARGIN < x < WIDTH - MARGIN and MARGIN < y < HEIGHT - MARGIN):
+            self.canvas.focus_set()
+
+            row, col = (y - MARGIN) / SIDE, (x - MARGIN) / SIDE
+            if (row, col) == (self.row, self.col):
+                self.row, self.col = -1, -1
+            elif self.game.puzzle[row][col] == 0:
+                self.row, self.col = row, col
+
+        self.__draw_cursor()
+
+    def __draw_cursor(self):
+        self.canvas.delete("cursor")
+        if self.row >= 0 and self.col >= 0:
+            x0 = MARGIN + self.col * SIDE + 1
+            y0 = MARGIN + self.row * SIDE + 1
+            x1 = MARGIN + (self.col + 1) * SIDE - 1
+            y1 = MARGIN + (self.row + 1) * SIDE - 1
+            self.canvas.create_rectangle(x0, y0, x1, y1, outline="red", tags="cursor")
+
+    def __key_pressed(self, event):
+        if self.game.game_over:
+            return
+        if self.row >= 0 and self.col >= 0 and event.char in "1234567890":
+            self.game.puzzle[self.row][self.col] = int(event.char)
+            self.col, self.row = -1, -1
+            self.__draw_puzzle()
+            self.__draw_cursor()
+            if self.game.check_win():
+                self.__draw_victory()
+
+    def __draw_victory(self):
+        x0 = y0 = MARGIN + SIDE * 2
+        x1 = y1 = MARGIN + SIDE * 7
+        self.canvas.create_oval(x0, y0, x1, y1, tags="victory", fill="dark orange", outline="orange")
+        x = y = MARGIN + 4 * SIDE + SIDE / 2
+        self.canvas.create_text(x, y, text="You win!", tags="winner", fill="white", font=("Arial", 32))
+
+if __name__ == '__main__':
+    board_name = parse_arguments()
+    with open('%s.sudoku' % board_name, 'r') as board_file:
+        game = SudokuGame(board_file)
+        game.start()
+
+        root = Tk()
+        SudokuUI(root, game)
+        root.geometry("%dx%d" % (WIDTH, HEIGHT + 40))
+        root.mainloop()
 
